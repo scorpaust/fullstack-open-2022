bdf2a7cac74bd9931f46525c53a309891cff176d
Dinis Costa <scorpaust@hotmail.com>
2022-10-09 21:25:43 +0100

    Part 3

--- part3/phonebook/phonebook_frontend/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-const Filter = ({newFilter, handleNewFilter}) => {
-    return (
-        <>
-            filter shown with <input value={newFilter} onChange={handleNewFilter} />
-        </>
-    )
-}
-
-export default Filter;
